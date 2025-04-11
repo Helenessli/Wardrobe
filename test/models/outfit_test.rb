b52dcaa886aa73e1aec8e4d3957bdb499f187bdc
Helen Li <helenessli6@gmail.com>
2025-04-11 10:52:22 -0400

    outfit model basic test

--- test/models/outfit_test.rb
+++ test/models/outfit_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class OutfitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create outfit with valid attributes" do
+    outfit = Outfit.new(theme: "summer", number: 1)
+    assert outfit.valid?
+  end
+
+  test "should not create outfit without number" do
+    outfit = Outfit.new(theme: "summer")
+    assert_not outfit.valid?
+  end
+
+  test "should create outfit with items" do
+    outfit = Outfit.new(
+      theme: "summer",
+      number: 1,
+      items_attributes: [
+        { name: "Summer Top", brand: "Lewkin" },
+        { name: "Summer Skirt", brand: "Lewkin" }
+      ]
+    )
+    assert outfit.valid?
+    assert_equal 2, outfit.items.size
+  end
 end
